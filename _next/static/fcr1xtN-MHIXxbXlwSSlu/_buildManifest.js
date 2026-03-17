8c4f03d18f6570b8e71e7ade8d756b8f1cd3cea2
Navin319931 <navinsharma9376319931@gmail.com>
2026-03-17 21:01:26 +0530

    Updates

--- _next/static/fcr1xtN-MHIXxbXlwSSlu/_buildManifest.js
+++ _next/static/fcr1xtN-MHIXxbXlwSSlu/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/ShreeKrupaBot//_next/:path+",
-        "destination": "/ShreeKrupaBot/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
